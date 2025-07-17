165332c83786bb071f77589f2a4ba7a5c1f62ecb
박홍렬 <vlhong94@gmail.com>
2025-07-17 11:16:29 +0900

    error stack

--- src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
@@ -15,35 +15,37 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException e) {
-        log.error(e.getMessage());
+        log.error("", e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).build();
     }
 
     @ExceptionHandler(ApplicationException.class)
     protected ResponseEntity<Object> applicationException(ApplicationException e) {
-        log.error(e.getMessage());
+        log.error("", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).build();
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<Object> accessDeniedException(AccessDeniedException e) {
+        log.error("", e);
         return ResponseEntity.status(HttpStatus.FORBIDDEN.value()).build();
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<Object> badCredentialsException(BadCredentialsException e) {
+        log.error("", e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED.value()).build();
     }
 
     @ExceptionHandler(RuntimeException.class)
     protected ResponseEntity<Object> runtimeException(RuntimeException e) {
-        log.error(e.getMessage());
+        log.error("", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).build();
     }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<Object> exception(Exception e) {
-        log.error(e.getMessage());
+        log.error("", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).build();
     }
 
