dc18461b4ae9b9bed33a4fd4b496c91bd2541a6b
parkhr <vlhong94@gmail.com>
2025-08-26 12:03:54 +0900

    fix: 모바일 반응형

--- frontend/record/src/layout/DefaultLayout.vue
+++ frontend/record/src/layout/DefaultLayout.vue
@@ -1,49 +1,79 @@
 <template>
   <div style="min-height: 100vh; width: 100vw; display: flex; flex-direction: column; overflow-x: hidden;">
-    <a-layout-header>
-      <a-menu
-        theme="dark"
-        mode="horizontal"
-        :selectedKeys="[selectedMenu]"
-        :style="{ lineHeight: '64px' }"
-      >
+    <a-row>
+      <a-col :xs="24" :sm="24" :md="24" :lg="24">
+        <a-layout-header>
+          <a-row>
+            <a-col :xs="0" :sm="0" :md="24">
+              <a-menu
+                theme="dark"
+                mode="horizontal"
+                :selectedKeys="[selectedMenu]"
+                :style="{ lineHeight: '64px' }"
+              >
+                <a-menu-item v-for="menu in menus" :key="menu.link" @click="onMenuClick(menu)">
+                  {{ menu.name }}
+                </a-menu-item>
+              </a-menu>
+            </a-col>
+            <a-col :xs="2" :sm="2" :md="0">
+              <a-button type="text" @click="drawerVisible = true" style="margin-top:16px;">
+                <MenuOutlined style="color:black; font-size:20px;" />
+              </a-button>
+            </a-col>
+          </a-row>
+        </a-layout-header>
+      </a-col>
+    </a-row>
+
+    <!-- 모바일 Drawer -->
+    <a-drawer
+      v-model:open="drawerVisible"
+      placement="left"
+      title="메뉴"
+      :closable="true"
+    >
+      <a-menu mode="inline" :selectedKeys="[selectedMenu]">
         <a-menu-item v-for="menu in menus" :key="menu.link" @click="onMenuClick(menu)">
           {{ menu.name }}
         </a-menu-item>
       </a-menu>
-    </a-layout-header>
+      <a-divider />
+      <a-menu mode="inline" :selectedKeys="[selectedChildMenu]">
+        <a-menu-item v-for="menu in childMenus" :key="menu.link" @click="onChildMenuClick(menu)">
+          {{ menu.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-drawer>
 
-    <div style="background: #fff;">
-      <a-row style="background: #fff;">
-        <a-col :span="2" style="background: #fff;">
-          <!-- 사이드 메뉴 -->
-          <a-menu mode="inline" :selectedKeys="[selectedChildMenu]">
-            <a-menu-item v-for="menu in childMenus" :key="menu.link" @click="onChildMenuClick(menu)">
-              {{ menu.name }}
-            </a-menu-item>
-          </a-menu>
-        </a-col>
+    <a-row style="background: #fff;">
+      <a-col :xs="0" :sm="0" :md="2">
+        <!-- 사이드 메뉴 -->
+        <a-menu mode="inline" :selectedKeys="[selectedChildMenu]">
+          <a-menu-item v-for="menu in childMenus" :key="menu.link" @click="onChildMenuClick(menu)">
+            {{ menu.name }}
+          </a-menu-item>
+        </a-menu>
+      </a-col>
 
-        <a-col :span="22">
-          <!-- 페이지 내용 -->
-          <a-row>
-            <a-col :span="21">
-              <!-- <a-page-header :title="selectedChildMenuName" @back="() => null" /> -->
-                <a-page-header :title="selectedChildMenuName"/>
-            </a-col>
-            <a-col :span="3">
-              <div style="display: flex; align-items: center; height: 100%;">
-                <a-breadcrumb>
-                  <a-breadcrumb-item>{{selectedMenuName}}</a-breadcrumb-item>
-                  <a-breadcrumb-item>{{selectedChildMenuName}}</a-breadcrumb-item>
-                </a-breadcrumb>
-              </div>
-            </a-col>
-          </a-row>
-          <router-view />
-        </a-col>
-      </a-row>
-    </div>
+      <a-col :xs="24" :sm="24" :md="22">
+        <!-- 페이지 내용 -->
+        <a-row>
+          <a-col :span="21">
+              <a-page-header :title="selectedChildMenuName"/>
+          </a-col>
+          <a-col :xs="0" :sm="0" :md="3">
+            <div style="display: flex; align-items: center; height: 100%;">
+              <a-breadcrumb>
+                <a-breadcrumb-item>{{selectedMenuName}}</a-breadcrumb-item>
+                <a-breadcrumb-item>{{selectedChildMenuName}}</a-breadcrumb-item>
+              </a-breadcrumb>
+            </div>
+          </a-col>
+        </a-row>
+        <router-view />
+      </a-col>
+    </a-row>
 
     <a-layout-footer style="text-align: center; width: 100%; margin-top: auto;">
       Ant Design ©2018 Created by Ant UED
@@ -56,6 +86,7 @@ import api from '@/api/axios'
 import { message } from 'ant-design-vue'
 import { onMounted, ref, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+import { MenuOutlined } from "@ant-design/icons-vue";
 
 const router = useRouter()
 
@@ -69,6 +100,8 @@ const selectedMenuName = ref('')
 const selectedChildMenu = ref('')
 const selectedChildMenuId = ref('')
 const selectedChildMenuName = ref('')
+
+const drawerVisible = ref(false);
 
 // 메뉴 클릭 시 key를 기준으로 라우팅
 const onMenuClick = async (menu) => {
@@ -120,6 +153,7 @@ const onChildMenuClick = (menu) => {
   localStorage.setItem('selectedChildMenuName', menu.name)
 
   router.push(menu.link)
+  drawerVisible.value = false;
 }
 
 onMounted(async () => {
